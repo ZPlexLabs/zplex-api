c89e5229c06531f225a29ac62e8131988ae1a9a2
zechs <vijay.vky2002@gmail.com>
2024-03-18 19:51:56 +0530

    Initial commit!

---
+++ src/main/java/zechs/zplex/api/ZPlexApiApplication.java
@@ -0,0 +1,13 @@
+package zechs.zplex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZPlexApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZPlexApiApplication.class, args);
+    }
+
+}
