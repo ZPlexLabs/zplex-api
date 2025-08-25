43e7ea78ad0fd6a25c2171d71a0f5feeb78c02fb
zechs <vijay.vky2002@gmail.com>
2025-08-25 20:03:39 +0530

    json for global exception handler

--- api/src/main/java/zechs/zplex/GlobalExceptionHandler.java
+++ api/src/main/java/zechs/zplex/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package zechs.zplex;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+import zechs.zplex.common.model.ErrorResponse;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,17 +18,25 @@ public class GlobalExceptionHandler {
     private static final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAllExceptions(Exception ex) {
-        logger.log(Level.SEVERE, "Unexpected error occurred", ex);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Something went wrong. Please try again later.");
+    public ResponseEntity<ErrorResponse> handleAllExceptions(Exception exception) {
+        logger.log(Level.SEVERE, "Unexpected error occurred", exception);
+        return switch (exception) {
+            case NoResourceFoundException ex -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(new ErrorResponse("The requested resource was not found."));
+
+            default -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(new ErrorResponse("Something went wrong. Please try again later."));
+        };
     }
 
     @ExceptionHandler(DataAccessException.class)
-    public ResponseEntity<String> handleDatabaseExceptions(DataAccessException ex) {
+    public ResponseEntity<?> handleDatabaseExceptions(DataAccessException ex) {
         logger.log(Level.WARNING, "Database error", ex);
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-                .body("Database temporarily unavailable.");
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorResponse("Database temporarily unavailable."));
     }
 
 }
