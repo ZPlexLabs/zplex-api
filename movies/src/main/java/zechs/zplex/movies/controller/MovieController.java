36dfda008bf2dfcbba979e41a7afe1bd1e88cb68
zechs <vijay.vky2002@gmail.com>
2025-09-07 17:27:41 +0530

    created movies module

---
+++ movies/src/main/java/zechs/zplex/movies/controller/MovieController.java
@@ -0,0 +1,9 @@
+package zechs.zplex.movies.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movie")
+public class MovieController {
+}
